5fd08fa0495934199e66c0eddc26657431aaa4c7
3ypup <posilay.pochashe@gmail.com>
2017-11-27 16:34:47 +0300

    add key (product_id)

--- public/cart.js
+++ public/cart.js
@@ -1,13 +1,13 @@
 function add_to_cart(id)
 {
+var key = "product_"+ id
 
-
-var x = window.localStorage.getItem('product_'+id);
+var x = window.localStorage.getItem(key);
 
 x = x*1+1
 
 
-window.localStorage.setItem('product_'+id, x)
+window.localStorage.setItem(key, x)
 
 alert ('product_'+id +"=" + x);
 	
