5dc464ace8cc50273d7ccba8b0e6eaa9d046a629
3ypup <posilay.pochashe@gmail.com>
2017-11-29 09:11:34 +0300

    add vie the cart

--- public/cart.js
+++ public/cart.js
@@ -14,18 +14,34 @@ alert ('product_'+id +"=" + x);
 }
 
 
-
-
-function view_the_cart()
+function view_the_desk()
 {
 
+	var cnt = 0;
+
+for(var i = 0; i < window.localStorage.length; i++ ) 
+	{
+		
+		var key = window.localStorage.key(i); 
+		
+		var value = window.localStorage.getItem(key);
+	
+
+	if (key.indexOf('product_')==0)
+	{
+		cnt=cnt+value*1;
+	}
+
+
+	}
 
 
 
-alert("there is your cart!")
 
+alert ("You ardered "+cnt+" pizzas!!")
 
 }
+
 
 function delete_to_cart()
 {
@@ -33,7 +49,7 @@ function delete_to_cart()
 
 localStorage.clear()
 
-alert ("Cart is clear!")
+alert ("Cart is clear ")
 
 	
 }
